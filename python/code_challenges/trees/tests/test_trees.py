40ea2430e452ba6ef483cc46779bf46a3eb08dad
belelelolahi <belalelolahi003@gmail.com>
2021-11-20 15:39:38 +0200

    code Challenge 15

--- python/code_challenges/trees/tests/test_trees.py
+++ python/code_challenges/trees/tests/test_trees.py
@@ -37,14 +37,14 @@ def test_instantiate_a_tree_with_a_single_root_node():
     assert actual == excepted
 
 def  test_add_a_left_child_and_right_child_to_a_single_root_node():
-    """ tree = BinarySearchTree()
-    tree.add(10)
-    tree.add(5)
-    tree.add(15)
+    tree1 = BinarySearchTree()
+    tree1.add(10)
+    tree1.add(5)
+    tree1.add(15)
 
-    actual = tree.pre_order(tree.root)
-    excepted = ['10','5','15']
-    assert actual == excepted """
+    actual =  [tree1.root.value,tree1.root.left.value,tree1.root.right.value]
+    excepted = [10,5,15]
+    assert actual == excepted
     pass
 
 
@@ -63,6 +63,8 @@ def test_return_a_collection_from_an_postorder_traversal(creat_tree):
     excepted =  ['D','B','F','E','C','A']
     actual = creat_tree.post_order(creat_tree.root)
     assert excepted == actual
+
+
 
 
 
--- python/code_challenges/trees/trees/tree.py
+++ python/code_challenges/trees/trees/tree.py
@@ -44,20 +44,45 @@ class BinarySearchTree(BinaryTree) :
     def __init__(self) -> None:
         super().__init__()
 
-    def add (self,value):
-        if self.root == None :
-            self.root = Node(value)
 
-        if value < self.root.value :
-
-
-                self.root.left = Node(value)
-
-
-        else:
-               self.root.left = Node(value)
+    def add(self,value):
+           new_Node = Node(value)
+           if self.root==None :
+                  self.root= new_Node
+           else:
+                  self._add(new_Node,self.root)
 
 
+    def _add(self, new_Node, root):
+           if(root.value > new_Node.value):
+                  if(root.left==None):
+                         root.left=new_Node
 
-    def contains(value):
-        pass
+                  else:
+                         self._add(new_Node,root.left)
+           else:
+                   if(root.right==None):
+                         root.right=new_Node
+                   else:
+                         self._add(new_Node,root.right)
+
+
+
+    def contains(self,value):
+        if self.root.value == value:
+            return True
+        else :
+            self._contains(value,self.root)
+
+    def  _contains(self,value,root):
+        if root.value > value :
+            if root.left.value == value :
+                return True
+            else :
+                self._contains(value,root.left)
+        else :
+            if root.right.value == value :
+                return True
+            else :
+                self._contains(value,root.right)
+
